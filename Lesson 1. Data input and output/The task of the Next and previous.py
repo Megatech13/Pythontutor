ce4b9dd7219571040370d19073ee6269b605926f
Megatech13 <megatec13@gmail.com>
2020-05-12 23:43:24 +0300

    новые решения

---
+++ Lesson 1. Data input and output/The task of the Next and previous.py
@@ -0,0 +1,6 @@
+# Условие
+# Напишите программу, которая считывает целое число и выводит текст, аналогичный приведенному в примере (пробелы важны!).
+
+number = int(input())
+
+print(f"The next number for the number {number} is {number+1}.\nThe previous number for the number {number} is {number-1}.")
