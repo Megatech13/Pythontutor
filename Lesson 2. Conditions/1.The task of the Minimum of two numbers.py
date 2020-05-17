6f6d4d99886ffeb51575bad59919d8d34312e363
Megatech13 <megatec13@gmail.com>
2020-05-17 15:03:36 +0300

    новые ответы

---
+++ Lesson 2. Conditions/1.The task of the Minimum of two numbers.py
@@ -0,0 +1,12 @@
+# The task of the "Minimum of two numbers»
+
+#Условие
+#Даны два целых числа. Выведите значение наименьшего из них.
+
+a = int(input())
+b = int(input())
+
+if a < b:
+    print(a)
+else:
+    print(b)
